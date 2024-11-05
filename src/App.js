8e0fe61d6538dba1f9ae0706963819d21d8bcbe7
=elijahizkiel <=>
2024-11-05 21:00:14 +0300

    add custom made components to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Header from './components/Header';
+import Note from './components/Note';
+import Footer from './components/Footer';
 import './App.css';
 
+const date = new Date(Date.now())
+
 function App() {
- 
+ return (
+  <>
+  <Header text="Keeper" />
+  <Note title="This is the note Title" text="Here is the content" />
+  <Footer text="Copy right reserved ©" year={date.getFullYear()} />
+  </>
+ );
 }
 
 export default App;
