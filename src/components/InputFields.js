678d2af07084843e2d379149028c3bad4b4a4070
=elijahizkiel <=>
2024-11-09 10:54:38 +0300

    enclose input elements into a div

--- src/components/InputFields.js
+++ src/components/InputFields.js
@@ -1,8 +1,8 @@
 function InputFields(props){
-    return <>
+    return <div className="note-text">
         <input type="text" className="title-input" id="title-input" value={props.titleValue} onChange={props.onTitleChange} />
         <textarea id="note-content" cols="30" rows="10" value={props.contentValue} onChange={props.onContentChange} />
-        </>
+        </div>
 }
 
 export default InputFields;
